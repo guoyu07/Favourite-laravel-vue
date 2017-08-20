2f4926808f188b0d8170c100416d0262ee589458
hoakusa <hoakusa@gmail.com>
2017-08-21 00:46:07 +0300

    like action

---
+++ app/Http/Controllers/PostsController.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use App\Post;
+
+class PostsController extends Controller
+{
+    //
+    public function index()
+    {
+        $posts = Post::paginate(5);
+
+        return view('posts.index', compact('posts'));
+    }
+
+    /**
+    * Favorite a particular post
+    *
+    * @param  Post $post
+    * @return Response
+    */
+    public function favoritePost(Post $post)
+    {
+        Auth::user()->favorites()->attach($post->id);
+
+        return back();
+    }
+
+    /**
+    * Unfavorite a particular post
+    *
+    * @param  Post $post
+    * @return Response
+    */
+    public function unFavoritePost(Post $post)
+    {
+        Auth::user()->favorites()->detach($post->id);
+
+        return back();
+    }
+}
---
+++ app/Post.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+use App\Favorite;
+use Illuminate\Support\Facades\Auth;
+
+class Post extends Model
+{
+    //
+    public function favorited()
+    {
+        return (bool) Favorite::where('user_id', Auth::id())
+                            ->where('post_id', $this->id)
+                            ->first();
+    }
+}
